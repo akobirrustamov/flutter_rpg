cc521925d42db1e40a0d20f68b5074980ea0561e
Akobir Rustamov <rustamovakobir8434@gmail.com>
2025-12-11 14:52:26 +0500

    second change

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const StyledTitle("Your characters")),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            StyledText("Character List"),
+            StyledHeading("Character List "),
+            StyledTitle("Character List "),
+
+            FilledButton(onPressed: () {}, child: const Text("Create new")),
+          ],
+        ),
+      ),
+    );
+  }
+}

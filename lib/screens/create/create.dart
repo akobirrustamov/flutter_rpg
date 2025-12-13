8606fff20274993c2b8cfe49b95cc90f601cbcdf
Akobir Rustamov <rustamovakobir8434@gmail.com>
2025-12-13 09:16:03 +0500

    45 lesson is ready

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/model/character.dart';
 import 'package:flutter_rpg/model/vocation.dart';
 import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = const Uuid();
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -24,20 +28,35 @@ class _CreateState extends State<Create> {
     super.dispose();
   }
 
+  // handling select vocation
+
+  Vocation selectedVocation = Vocation.junkie;
+
+  void updateVocation(Vocation vocation) {
+    setState(() {
+      selectedVocation = vocation;
+    });
+  }
+
   // handler
   void handleSubmit() {
     if (_nameController.text.trim().isEmpty) {
-      print("name is empty");
+      //error dialog
       return;
     }
     if (_sloganController.text.trim().isEmpty) {
-      print("name is empty");
+      //error dialog
       return;
     }
-    
-    print(_nameController.text);
-    print(_sloganController.text);
 
+    characters.add(
+      Character(
+        name: _nameController.text.trim(),
+        slogan: _sloganController.text.trim(),
+        vocation: selectedVocation,
+        id: uuid.v4(),
+      ),
+    );
   }
 
   @override
@@ -49,79 +68,95 @@ class _CreateState extends State<Create> {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: Column(
-          children: [
-
-            // welcome
-            Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
-            const Center(child: StyledHeading("Welcome, new Shogird")),
-            const Center(
-              child: StyledText(
-                "Create, a name & new slogan for your shogird.",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // welcome
+              Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+              const Center(child: StyledHeading("Welcome, new Shogird")),
+              const Center(
+                child: StyledText(
+                  "Create, a name & new slogan for your shogird.",
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
+              const SizedBox(height: 30),
 
-
-            // input name & slogan
-            TextField(
-              controller: _nameController,
-              style: GoogleFonts.kanit(
-                textStyle: Theme.of(context).textTheme.bodyMedium,
+              // input name & slogan
+              TextField(
+                controller: _nameController,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person_2),
+                  label: StyledText("Shogird name"),
+                ),
               ),
-              cursorColor: AppColors.textColor,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.person_2),
-                label: StyledText("Shogird name"),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _sloganController,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.chat),
+                  label: StyledText("shogird slogan"),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _sloganController,
-              style: GoogleFonts.kanit(
-                textStyle: Theme.of(context).textTheme.bodyMedium,
+              SizedBox(height: 30),
+
+              // select vocation title
+              Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+              const Center(child: StyledHeading("Choose name vocation")),
+              const Center(
+                child: StyledText("This determins your available skills"),
               ),
-              cursorColor: AppColors.textColor,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.chat),
-                label: StyledText("shogird slogan"),
+              const SizedBox(height: 30),
+
+              // vocation cards
+              VocationCard(
+                selected: selectedVocation == Vocation.junkie,
+                onTap: updateVocation,
+                vocation: Vocation.junkie,
               ),
-            ),
-            SizedBox(height: 30),
+              VocationCard(
+                selected: selectedVocation == Vocation.ninja,
+                onTap: updateVocation,
+                vocation: Vocation.ninja,
+              ),
+              VocationCard(
+                selected: selectedVocation == Vocation.raider,
+                onTap: updateVocation,
+                vocation: Vocation.raider,
+              ),
+              VocationCard(
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.wizard,
+                vocation: Vocation.wizard,
+              ),
+              const SizedBox(height: 40),
 
 
-            // select vocation title 
-            Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
-            const Center(child: StyledHeading("Choose name vocation")),
-            const Center(
-              child: StyledText(
-                "This determins your available skills",
+               // welcome
+              Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+              const Center(child: StyledHeading("Omad!")),
+              const Center(
+                child: StyledText(
+                  "Shgird yaratishdan to'xtama...",
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
+              const SizedBox(height: 30),
 
-
-            // vocation cards
-            const VocationCard(
-              vocation: Vocation.junkie
-            ),
-            const VocationCard(
-              vocation: Vocation.ninja
-            ),
-            const VocationCard(
-              vocation: Vocation.raider
-            ),
-            const VocationCard(
-              vocation: Vocation.wizard
-            ),
-
-            Center(
-              child: StyledButton(
-                onPressed: handleSubmit,
-                child: StyledHeading("Create shogird"),
+              Center(
+                child: StyledButton(
+                  onPressed: handleSubmit,
+                  child: StyledHeading("Create shogird"),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
